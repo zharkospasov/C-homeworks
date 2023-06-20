5cb40e710a69bdc0fa128dfc4571a81ec7cb88b1
zharkospasov <zharkospasov@gmail.com>
2023-06-20 22:46:50 +0300

    added 08.

---
+++ 01. FirstStepsInCodingExercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07.FoodDelivery
+{
+    class Program
+    {
+        static void Main()
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int veganMenu = int.Parse(Console.ReadLine());
+
+            double result = chickenMenu * 10.35 + fishMenu * 12.40 + veganMenu * 8.15;
+            result = result + result * 0.20 + 2.50;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01. FirstStepsInCodingExercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08.BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yearlyPrice = double.Parse(Console.ReadLine());
+            double snickers = yearlyPrice - yearlyPrice * 0.40;
+            double clothes = snickers - snickers * 0.20;
+            double ballPrice = clothes / 4;
+            double accessories = ballPrice / 5;
+            double result = yearlyPrice + snickers + clothes + ballPrice + accessories;
+
+            Console.WriteLine(result);
+        }
+    }
+}
