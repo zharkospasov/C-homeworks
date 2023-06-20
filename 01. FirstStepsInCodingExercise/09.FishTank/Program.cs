817c135785cce1a3480032aba5e0ff617b30c6a0
zharkospasov <zharkospasov@gmail.com>
2023-06-20 22:59:14 +0300

    uploaded last unit of homework

---
+++ 01. FirstStepsInCodingExercise/09.FishTank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09.FishTank
+{
+    class Program
+    {
+        static void Main()
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double occupiedVolume = double.Parse(Console.ReadLine());
+            double volume = lenght * width * height;
+            volume = volume * 0.001;
+            volume = volume - volume * occupiedVolume/100;
+            Console.WriteLine(volume);
+        }
+    }
+}
