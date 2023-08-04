dc373888d5c222f2fb379669bd210db2454d29ce
zharkospasov <zharkospasov@gmail.com>
2023-08-04 14:47:48 +0300

    done!
    While loop done all!

--- WhileLoop-Exercisess/07.Moving/Program.cs
+++ WhileLoop-Exercisess/07.Moving/Program.cs
@@ -4,9 +4,26 @@ namespace _07.Moving
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int volume = width * length * height;
+
+            string input = Console.ReadLine();
+            while (input!="Done")
+            {
+                int boxes = int.Parse(input);
+                volume -= boxes;
+                if (volume<=0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+                    return;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{volume} Cubic meters left.");
         }
     }
 }
