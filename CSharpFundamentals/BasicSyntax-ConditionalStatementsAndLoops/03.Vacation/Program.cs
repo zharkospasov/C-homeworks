9dc2245902f07705fb036f477f2ca750a02ed711
zharkospasov <zharkospasov@gmail.com>
2023-08-08 17:17:57 +0300

    started new chapter
    Fundamental exercises

---
+++ CSharpFundamentals/BasicSyntax-ConditionalStatementsAndLoops/03.Vacation/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string day = Console.ReadLine();
+            double ticketPrice = 0;
+            double total = 0;
+            if (type=="Students")
+            {
+                if (day=="Friday")
+                {
+                    ticketPrice = 8.45;
+                    total = ticketPrice * n;
+                    if (n>=30)
+                    {
+                        total -= total * 0.15;
+                    }
+                }
+                if (day=="Saturday")
+                {
+                    ticketPrice = 9.80;
+                    total = ticketPrice * n;
+                    if (n >= 30)
+                    {
+                        total -= total * 0.15;
+                    }
+                }
+                if (day=="Sunday")
+                {
+                    ticketPrice = 10.46;
+                    total = ticketPrice * n;
+                    if (n >= 30)
+                    {
+                        total -= total * 0.15;
+                    }
+                }
+            }
+            if (type == "Business")
+            {
+                if (day == "Friday")
+                {
+                    ticketPrice = 10.90;
+                    total = ticketPrice * n;
+                    if (n >= 100)
+                    {
+                        total -= ticketPrice*10;
+                    }
+                }
+                if (day == "Saturday")
+                {
+                    ticketPrice = 15.60;
+                    total = ticketPrice * n;
+                    if (n >= 100)
+                    {
+                        total -= ticketPrice * 10;
+                    }
+                }
+                if (day == "Sunday")
+                {
+                    ticketPrice = 16;
+                    total = ticketPrice * n;
+                    if (n >= 100)
+                    {
+                        total -= ticketPrice * 10;
+                    }
+                }
+            }
+            if(type=="Regular")
+            {
+                if (day == "Friday")
+                {
+                    ticketPrice = 15;
+                    total = ticketPrice * n;
+                    if (n >= 10 && n <= 20)
+                    {
+                        total -= total * 0.05;
+                    }
+                }
+                if (day == "Saturday")
+                {
+                    ticketPrice = 20;
+                    total = ticketPrice * n;
+                    if (n >=10&&n<=20)
+                    {
+                        total -= total*0.05;
+                    }
+                }
+                if (day == "Sunday")
+                {
+                    ticketPrice = 22.50;
+                    total = ticketPrice * n;
+                    if (n >= 100)
+                    {
+                        total -= ticketPrice * 10;
+                    }
+                }
+            }
+            Console.WriteLine($"Total price: {total:f2}");
+        }
+    }
+}
---
+++ CSharpFundamentals/BasicSyntax-ConditionalStatementsAndLoops/04.PrintAndSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.PrintAndSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int x = 0;
+            
+            for (int i = n; i <= m; i++)
+            {
+                Console.Write(i+" ");
+                x += i;
+
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {x}");
+        }
+    }
+}
